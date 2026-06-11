125da022ce7cd90e986ba16bd7b41b305d53546d
miranda9932 <albertmirandabayes@gmail.com>
2026-06-11 13:28:03 +0200

    Exercise 5 completed

--- src/app/exercises/04-http/exercise-04-http.component.ts
+++ src/app/exercises/04-http/exercise-04-http.component.ts
@@ -1,8 +1,9 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { catchError, of } from 'rxjs';
+import { catchError, filter, map, of } from 'rxjs';
 import { UserService } from '../../core/user.service';
 import { UserListComponent } from '../../shared/user-list.component';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-exercise-04-http',
@@ -13,8 +14,11 @@ import { UserListComponent } from '../../shared/user-list.component';
 })
 export class Exercise04HttpComponent {
   private readonly userService = inject(UserService);
+  requestLog = '';
 
   readonly users$ = this.userService.getUsers().pipe(
+    map(users => users.filter(user => user.active == false)),
+    tap(() => this.requestLog= 'order confirmed'),
     catchError(() => of([]))
   );
 }
