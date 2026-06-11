cf36204aace5a2b19a306ced47e5b4aafb39cec2
miranda9932 <albertmirandabayes@gmail.com>
2026-06-11 17:51:37 +0200

    Add & implemet filters for Panel C and D

--- src/app/exercises/05-cache/exercise-05-cache.component.ts
+++ src/app/exercises/05-cache/exercise-05-cache.component.ts
@@ -2,6 +2,7 @@ import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../core/user.service';
 import { UserListComponent } from '../../shared/user-list.component';
+import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-exercise-05-cache',
@@ -16,17 +17,29 @@ export class Exercise05CacheComponent {
   readonly lastRequestNumber$ = this.userService.lastRequestNumber$;
   cachedUsers$ = this.userService.getUsersCached();
 
-  newobservable$ = this.userService.getUsersCached();
-  secondObservable$ = this.userService.getUsersCached();
+  adminObservableList$ = this.userService.getUsersCached();
+  activeObservableList$ = this.userService.getUsersCached();
 
   resetCachedObservable(): void {
     this.cachedUsers$ = this.userService.getUsersCached();
+  }
+  
+   resetCachedObservableAdmin(): void {
+    this.adminObservableList$ = this.userService.getUsersCached().pipe(
+      map(users => users.filter(user => user.role === 'admin'))
+    );
+  }
+
+   resetCachedObservableActive(): void {
+    this.activeObservableList$ = this.userService.getUsersCached().pipe(
+      map(users => users.filter(user => user.active === true))
+    );
   }
 
   clearServiceCache(): void {
     this.userService.clearUsersCache();
     this.cachedUsers$ = this.userService.getUsersCached();
-    this.newobservable$ = this.userService.getUsersCached();
-    this.secondObservable$ = this.userService.getUsersCached();
+    this.adminObservableList$ = this.userService.getUsersCached();
+    this.activeObservableList$ = this.userService.getUsersCached();
   }
 }
